39d0d0f74a57c08dab861dbba92fb4b0d17d229d
unknown <zubairwastibaloch@gmail.com>
2023-05-18 08:50:15 +0500

    Backend js file updated

--- Component/Backend.js
+++ Component/Backend.js
@@ -1,1 +1,2 @@
-export   const API= process.ENV_NODE === "production"? process.env.BASE_URL: process.env.LOCAL_URL
+  const API= process.ENV_NODE === "production"? process.env.BASE_URL: process.env.LOCAL_URL
+  export default API
