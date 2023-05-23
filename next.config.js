15630691f6b143197beab685c93330528439365e
unknown <zubairwastibaloch@gmail.com>
2023-05-23 16:09:59 +0500

    nextConfig is modified

--- next.config.js
+++ next.config.js
@@ -6,3 +6,13 @@ const nextConfig = {
 
 module.exports = nextConfig
 
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://e-commerce-next-ap-5vp198yct-zubairahmed22.vercel.app/:path*'
+      }
+    ]
+  }
+}
