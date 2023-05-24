8a338dd186f1ff5ee42db8dafbca0fbe11935b06
unknown <zubairwastibaloch@gmail.com>
2023-05-24 10:13:25 +0500

    bug are fixed

--- pages/login.js
+++ pages/login.js
@@ -18,7 +18,7 @@ import API from '../Component/Backend';
    const loginSubmit = async(e) =>{
     try {
       const  data = {email,password}
-      const API=process.env.NODE_ENV != "production"? "http://localhost:3000" : "https://e-commerce-next-ap-f5tk8s28p-zubairahmed22.vercel.app/"
+      const API=process.env.NODE_ENV != "production"? "http://localhost:3000" : "https://e-commerce-next-ap-f5tk8s28p-zubairahmed22.vercel.app"
       const response = await fetch(`${API}/api/login`,{
         method:"POST",
         headers: {
